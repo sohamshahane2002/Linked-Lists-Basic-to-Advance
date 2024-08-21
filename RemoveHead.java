cbacefc14083955b42b882a468363e010430d402
sohamshahane2002 <134382971+sohamshahane2002@users.noreply.github.com>
2024-08-22 00:55:44 +0530

    Add files via upload

---
+++ RemoveHead.java
@@ -0,0 +1,41 @@
+package LinkedLists.SinglyLinkedList.Deletion;
+
+class Node{
+    int data ;
+    Node next ;
+
+    Node(int data1 , Node next1){
+        this.data = data1;
+        this.next = next1;
+    }
+    Node(int data1){
+        this.data = data1;
+        this.next =null;
+    }
+}
+public class RemoveHead {
+
+    private static void printLL (Node head){
+        while(head!=null){
+            System.out.print(head.data + " ");
+            head = head.next;
+        }
+        System.out.println();
+    }
+    private static Node removeHead(Node head){
+        if( head == null) return head;
+        head = head.next;
+        return head;
+    }
+    public static void main(String[] args) {
+        int arr[] = {1,2,3,4};
+        Node head = new Node(arr[0]);
+        head.next = new Node(arr[1]);
+        head.next.next = new Node(arr[2]);
+        head.next.next.next = new Node(arr[3]);
+
+        head = removeHead(head);
+        printLL(head);
+    }
+    
+}
