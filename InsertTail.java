d7be1461f2ef8d5941cd14cdcf82e7451a99435e
sohamshahane2002 <134382971+sohamshahane2002@users.noreply.github.com>
2024-09-04 23:14:40 +0530

    Add files via upload

--- InsertTail.java
+++ InsertTail.java
@@ -1,50 +1,59 @@
-package LinkedLists.SinglyLinkedList.Insertion;
-class Node {
-    public int data;
-    public Node next;
+package LinkedLists.DoublyLinkedList.Insertion;
 
-    public Node(int data1, Node next1) {
-        data = data1;
-        next = next1;
-    }
-    public Node(int data1) {
-        data = data1;
-        next = null;
-    }
-}
 public class InsertTail {
-    public static void printLL(Node head) {
-        while (head != null) {
-            System.out.print(head.data + " ");
-            head = head.next;
+    public static class Node {
+        public int data;
+        public Node next;
+        public Node back;
+
+        // Constructor for a Node with both data, a reference to the next node, and a reference to the previous node
+        public Node(int data1, Node next1, Node back1) {
+            data = data1;
+            next = next1;
+            back = back1;
+        }
+        // Constructor for a Node with data, and no references to the next and previous nodes (end of the list)
+        public Node(int data1) {
+            data = data1;
+            next = null;
+            back = null;
         }
     }
-    public static Node insertTail(Node head, int val) {
-        // Create a new node for the element
-        Node newNode = new Node(val);
-        // If the list is empty, set the new node as the head
+    private static void print(Node head) {
+        while (head != null) {
+            System.out.print(head.data + " ");
+            head = head.next; 
+        }
+        System.out.println();
+    }
+    private static Node insertAtTail(Node head, int k) {
+        Node newNode = new Node(k);
         if (head == null) {
             return newNode;
         }
-        // Traverse to the last node
         Node current = head;
         while (current.next != null) {
             current = current.next;
         }
-        // Set the next pointer of the last node to the new node
-        current.next = newNode; 
-        return head; // Head remains the same
-    }
+        current.next = newNode;
+        newNode.back = current;
+        return head;
+        }
     public static void main(String[] args) {
-        int arr[] = {12, 8, 5, 7};
-        int val = 100;
-
+        int[] arr = {12, 5, 6, 8};
+        
         Node head = new Node(arr[0]);
         head.next = new Node(arr[1]);
         head.next.next = new Node(arr[2]);
         head.next.next.next = new Node(arr[3]);
+        
+        System.out.println("Doubly Linked List Initially: ");
+        print(head);
 
-        head = insertTail(head, val); // Insert at tail
-        printLL(head);
+        System.out.println("Doubly Linked List After Inserting before the node with value 8:");
+
+        head = insertAtTail(head, 10);
+        print(head);
+
     }
 }
